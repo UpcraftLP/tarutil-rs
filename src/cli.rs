ce46b8caa9b2c5826996a4120e8ddde04f3b9af5
Up <upcraftedlp@gmail.com>
2023-12-27 00:55:44 +0100

    Initial version

---
+++ src/cli.rs
@@ -0,0 +1,33 @@
+use std::path::PathBuf;
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(
+    name = env!("CARGO_PKG_NAME"),
+    version = env!("CARGO_PKG_VERSION"),
+    author = env!("CARGO_PKG_AUTHORS"),
+    about = env!("CARGO_PKG_DESCRIPTION"),
+)]
+pub(crate) struct Args {
+
+    #[arg(short = 'o', long)]
+    pub output: Option<PathBuf>,
+
+    #[arg(last = true)]
+    pub input: PathBuf,
+
+    #[arg(long)]
+    pub mapping_file: Option<PathBuf>,
+
+    #[arg(long)]
+    pub errors_file: Option<PathBuf>,
+
+    #[arg(long, default_value = "false")]
+    pub clean_paths: bool,
+
+    #[arg(long)]
+    pub remove_prefix: Option<String>,
+
+    #[arg(long, default_value = "()[]{}-+*=&@!?'#$%^~^´`:,;<>|\"\\")]
+    pub filter_chars: String,
+}
---
+++ src/main.rs
@@ -0,0 +1,135 @@
+mod cli;
+
+use std::{fs, io};
+use std::ffi::OsString;
+use std::io::prelude::*;
+use std::fs::File;
+use std::io::BufWriter;
+use std::time::Instant;
+
+use bimap::{BiHashMap, BiMap};
+use clap::Parser;
+use flate2::read::GzDecoder;
+use indicatif::{HumanDuration, ProgressBar};
+use tar::Archive;
+
+fn main() -> anyhow::Result<()> {
+    let now = Instant::now();
+    let args = cli::Args::parse();
+
+    let replacements = args.filter_chars.chars().collect::<Vec<char>>();
+
+    let tar_gz = File::open(&args.input)?;
+
+    let mut tar = Archive::new(GzDecoder::new(&tar_gz));
+    let archive_size = tar_gz.metadata()?.len();
+
+    let mut paths: BiMap<String, String> = BiHashMap::new();
+
+    let mut mapping_file = args.mapping_file.map(|p| BufWriter::new(File::create(p).expect("Failed to create mapping file")));
+    let mut errors: Vec<String> = Vec::new();
+
+    println!("Processing archive...");
+    let pb = ProgressBar::new(archive_size)
+        .with_style(
+            indicatif::ProgressStyle::default_bar()
+                .template("{spinner:.green} [{bar:150.cyan/blue}] {percent}% ({bytes_per_sec}, ETA: {eta}) {msg}").ok().unwrap()
+                .progress_chars("#>-"),
+        );
+
+    for mut entry in tar.entries()?
+        .filter_map(|e| e.ok())
+        .filter(|e| e.header().entry_type().is_file())
+    {
+        let p = entry.path()?;
+        if let Some(mut original) = p.to_str().map(|s| s.to_string()) {
+            // skip the original file
+            if OsString::from(&original) == args.input.file_name().unwrap() {
+                continue;
+            }
+
+            if !original.is_ascii() {
+                eprintln!("Skipping non-ascii path: {original}");
+                errors.push(format!("Skipping non-ascii path: {original}"));
+                continue;
+            }
+            if paths.contains_left(&original) {
+                eprintln!("Skipping duplicate path: {original}");
+                errors.push(format!("Skipping duplicate path: {original}"));
+                continue;
+            }
+
+            let mut lowercase = original.to_ascii_lowercase();
+            if let Some(prefix) = &args.remove_prefix {
+                if let Some(stripped) = lowercase.strip_prefix(prefix) {
+                    lowercase = stripped.to_string();
+                }
+            }
+
+            if args.clean_paths {
+                lowercase = lowercase
+                    .trim()
+                    .trim_start_matches(|c| c == '.')
+                    .trim_end_matches(|c| c == '.')
+                    .chars().filter(|c| !replacements.contains(c))
+                    // replace spaces with underscores if they are not already filtered out
+                    .map(|c| if c == ' ' { '_' } else { c })
+                    .collect::<String>().replace("__", "_");
+            }
+
+            if !lowercase.contains('.') {
+                eprintln!("Skipping path without extension: {lowercase}");
+                errors.push(format!("Skipping path without extension: {lowercase}"));
+                continue;
+            }
+
+            while paths.contains_right(&lowercase) {
+                lowercase = format!("_{}", lowercase);
+            }
+
+            paths.insert(original.to_string(), lowercase.clone());
+
+            if original.contains(' ') {
+                original = format!("'{original}'");
+            }
+
+            if lowercase.contains(' ') {
+                lowercase = format!("'{}'", lowercase);
+            }
+
+            if let Some(buffer) = mapping_file.as_mut() {
+                writeln!(buffer, "{original} {lowercase}")?;
+            }
+
+            if let Some(path) = &args.output {
+                let out_path = path.join(lowercase);
+                if let Some(parent) = out_path.parent() {
+                    fs::create_dir_all(parent)?;
+                }
+
+                let mut out_file = File::create(out_path)?;
+
+                io::copy(entry.by_ref(), &mut out_file)?;
+            }
+        }
+
+        let seek = tar_gz.try_clone()?.stream_position()?;
+        pb.set_position(seek);
+    }
+    pb.finish_and_clear();
+    println!("Done! ({})", HumanDuration(now.elapsed()));
+
+    if !errors.is_empty() {
+        if let Some(error_path) = &args.errors_file {
+            if let Some(path) = error_path.parent() {
+                fs::create_dir_all(path)?;
+            }
+            let mut errors_file = BufWriter::new(File::create(error_path)?);
+            for error in errors {
+                writeln!(errors_file, "{error}")?;
+            }
+        }
+    }
+
+    Ok(())
+}
